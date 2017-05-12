2d8dacbf8f8ae0873a0819f11a29eac8039fea55
Raphaël Dubigny <rdubigny@gmail.com>
2017-05-12 15:16:24 +0200

    use express router pattern

--- test/server.js
+++ test/server.js
@@ -1,29 +1,30 @@
-var expect = require('expect.js')
+var expect = require('expect.js');
 var request = require('supertest');
 var healthcheck = require('../source');
+var express = require('express');
 
 describe('server', function() {
 
   it('should use default urn', function(done) {
-    request(healthcheck())
+    request(express().use(healthcheck()))
       .get('/api/health-check')
       .expect(200, done);
   });
 
   it('should use specified urn', function(done) {
-    request(healthcheck({urn: '/coucou'}))
+    request(express().use(healthcheck({urn: '/coucou'})))
       .get('/coucou')
       .expect(200, done);
   });
 
   it('should return 200 when calling health-check-vip', function(done) {
-    request(healthcheck())
+    request(express().use(healthcheck()))
       .get('/api/health-check-vip')
       .expect(200, done);
   });
 
   it('should return 200 when calling health-check-vip with specified urn', function(done) {
-    request(healthcheck({urn: '/coucou'}))
+    request(express().use(healthcheck({urn: '/coucou'})))
       .get('/coucou-vip')
       .expect(200, done);
   });
